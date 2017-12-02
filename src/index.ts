bcd7cff33b32202ba9ef60fa6e2fe962450495db
patdaburu <pat@daburu.net>
2017-12-02 13:04:53 -0600

    Added typedoc support.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Accept a string and return it as is or LOUDER.
+ * @param {string} msg the original message
+ * @param {boolean} loud Do you want it returned LOUDER?
+ * @returns {string} the original message, or the LOUDER version
+ */
 export function echo(msg: string, loud: boolean = false): string {
 	if (!msg) {
 		return "";
